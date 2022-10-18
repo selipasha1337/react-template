0450d3131ed873eacc1b4f54ef189e01b5b91004
selipasha1337 <106588729+selipasha1337@users.noreply.github.com>
2022-10-18 21:57:20 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <p>React Template</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
